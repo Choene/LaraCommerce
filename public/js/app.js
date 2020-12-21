e7b86b572e72e876d9acd89059148db2ab392199
Choene <acchokoe@gmail.com>
2020-12-21 09:15:06 +0200

    Dev update

--- public/js/app.js
+++ public/js/app.js
@@ -21338,8 +21338,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\KTKN\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\KTKN\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\xampp\htdocs\LaraCommerce\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\LaraCommerce\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
